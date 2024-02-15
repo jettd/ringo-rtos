8937a517033d80bd901f4c99a50d55ac6517dff0
Darryl York III <yorkd@carleton.edu>
2024-02-15 17:15:35 -0600

    Lab2 updates

--- main/main.ino
+++ main/main.ino
@@ -2,59 +2,71 @@
 #include "RingoHardware.h"
 
 //int i;    //declaire any global variables here
+char edge;
+TaskHandle_t xRandomWalkHandle;
+TaskHandle_t xTaskRickRollHandle;
 
 void taskLights( void *pvParameters);
 void taskFancyLights(void *pvParameters);
 void taskRickRoll(void *pvParameters);
+void taskSense(void *pvParameters);
+void taskIRLights(void *pvParameters);
 
 void setup(){
   HardwareBegin();        //initialize Ringo's brain to work with his circuitry
-  PlayStartChirp();       //Play startup chirp and blink eyes
+  // PlayStartChirp();       //Play startup chirp and blink eyes
   SwitchMotorsToSerial(); //Call "SwitchMotorsToSerial()" before using Serial.print functions as motors & serial share a line
   RestartTimer();
-  Serial.begin(9600);     //Starts Serial Connection      
+  RxIRRestart(4);
+  // Serial.begin(9600);     //Starts Serial Connection  
+  ResetLookAtEdge();      //Zero LookAtEdge() avg     
 
-  // xTaskCreate(TaskLights, "rainbow", 128, NULL 2, NULL);
+  xTaskCreate(TaskLights, "rainbow", 128, NULL, 2, NULL);
+  xTaskCreate(TaskRandomWalk, "rwalk", 128, NULL, 1, &xRandomWalkHandle);
   // xTaskCreate(TaskFancyLights, "lights", 128, NULL, 5, NULL);
-  // xTaskCreate(TaskRickRoll, "rick", 128, NULL, 10, NULL);
-  xTaskCreate(TaskSense, "sense", 128, NULL, 10, NULL);
+  xTaskCreate(TaskRickRoll, "rick", 128, NULL, 3, &xTaskRickRollHandle);
+  // vTaskSuspend(xTaskRickRollHandle);
+  xTaskCreate(TaskSense, "sense", 128, NULL, 11, NULL);
+  // xTaskCreate(TaskIRLights, "ir", 128, NULL, 15, NULL);
 }
 
-void TaskSense(void *pvParameters) { // Aperiodic, how do we make them aperiodic?
+void TaskIRLights(void *pvParameters) {
   (void) pvParameters;
-  digitalWrite(Source_Select, HIGH); // Select sensors on top(HIGH) or bottom(LOW) of ringo
-  int frontSensor;
-  int rearSensor;
-  // for(;;){ // Testing
-  //   frontSensor = analogRead(LightSense_Left);
-  //   rearSensor = analogRead(LightSense_Rear);  // Select front and rear sensors
-  //   if ((frontSensor + 10 >= rearSensor) && (frontSensor - 10 <= rearSensor)) {PlayStartChirp();}
-  //   Serial.println(frontSensor);
-  //   Serial.println(rearSensor);
-  //   Serial.println();
-  //   vTaskDelay(2000/portTICK_PERIOD_MS);
-  // }
-  
-  frontSensor = analogRead(LightSense_Left);
-  rearSensor = analogRead(LightSense_Rear);  // Select front and rear sensors
-  if((frontSensor + 100 >= rearSensor) && (frontSensor - 100 >= rearSensor)){ // Differing range of sensor vals
-    for(;;){
-      frontSensor = analogRead(LightSense_Left);
-      rearSensor = analogRead(LightSense_Rear);  // Select front and rear sensors
-      Serial.println(frontSensor);
-      Serial.println(rearSensor);
-      Serial.println();
-      // move tf back until sensor values are the same
-      Motors(-80,-80);
-      vTaskDelay(250/portTICK_PERIOD_MS);
-      if((frontSensor + 100 >= rearSensor) && (frontSensor - 100 <= rearSensor)){
-        Motors(0,0);
-        break;
+  for(;;) {
+    if(IsIRDone()) {
+      cli();
+      byte ir_button = GetIRButton();
+      if(ir_button == 1) {
+        SetAllPixelsRGB(100, 100, 100);
+        vTaskResume(xTaskRickRollHandle);
+      } else if (ir_button == 19) {
+        SetPixelRGB(TAIL_BOTTOM, 0, 100, 0);
+      } else if (ir_button == 20) {
+        SetPixelRGB(TAIL_BOTTOM, 0, 0, 100);
       }
+      sei();
     }
+    vTaskDelay(100/portTICK_PERIOD_MS);
   }
-  TickType_t xLastTimeVal = xTaskGetTickCount();
-  vTaskDelayUntil(&xLastTimeVal, 250/portTICK_PERIOD_MS); // How do we make this aperiodic instead of polling?? Seperate check task with semaphore for this one. 
+}
+
+void TaskSense(void *pvParameters) { // Aperiodic handler task when we sense somemthing
+  (void) pvParameters;
+  for(;;){
+    edge = LookForEdge();
+    vTaskDelay(100/portTICK_PERIOD_MS);
+    if FrontEdgeDetected(edge){
+      SetAllPixelsRGB(0,100,100);
+      vTaskSuspend(xRandomWalkHandle);         // Suspend random walk
+      Motors(-50,-50);
+      delay(1000);
+      vTaskResume(xRandomWalkHandle);         // Restart running random walk
+    } else{
+      SetAllPixelsRGB(100,0,0);
+    }
+
+    vTaskDelay(100/portTICK_PERIOD_MS);    
+  }
 }
 
 void TaskLights(void *pvParameters) { // Periodic Task
@@ -95,7 +107,7 @@ void TaskLights(void *pvParameters) { // Periodic Task
   }
 }
 
-void TaskRickRoll(void *pvParameters) { // Sporadic Task
+void TaskRickRoll(void *pvParameters) { // Sporadic Task, don't respond to button press until song is over 
   (void) pvParameters;
   for(;;) {
     int bpm = 114; //reference
@@ -105,7 +117,7 @@ void TaskRickRoll(void *pvParameters) { // Sporadic Task
     int e_note = note/8;
     int s_note = note/16;
     double dot = 1.5;
-    int amp = 80;
+    int amp = 20;
 
     //s s s s e. e. q. 
     // never gonna give you up
@@ -339,13 +351,14 @@ void TaskFancyLights(void *pvParameters) {
   }
 }
 
-void TaskRandomWalk(void *pvParameters) {
+void TaskRandomWalk(void *pvParameters) { 
   TickType_t xLastWakeTime = xTaskGetTickCount();
+
   for(;;) {
-    int l = random(20, 100);
-    int r = random(20, 100);
+    int l = random(20, 50);
+    int r = random(20, 50);
     Motors(l, r);
-    vTaskDelayUntil(&xLastWakeTime, 1000/portTICK_PERIOD_MS);
+    vTaskDelayUntil(&xLastWakeTime, 500/portTICK_PERIOD_MS);
   }
 }
 
