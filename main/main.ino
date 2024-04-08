05ce80ffbbbd0be4ad636b7f40a6f157f864969c
Jett Durante <jdurante2@macbook-pro-153.unl.edu>
2024-04-07 19:38:21 -0500

    submission ready

--- main/main.ino
+++ main/main.ino
@@ -74,53 +74,6 @@ void TaskMotorControl(void *pvParameters) {
   }
 }
 
-// void TaskNav(void* pvParameters) {
-//   bool edge_reset_flag = false;
-//   int ramp_turn_radius = 2;
-//   int until_ramp_counter = 100;
-//   char edge;
-//   int LeftAverageHold, RightAverageHold;
-//   int loopHold = 15; //10 works good
-//   int left_speed = 60;
-//   int right_speed = 70;
-//   int turn_diff = 50;
-//   int max_allowed = 125;
-//   int min_allowed = 0;
-//   SwitchAmbientToEdge();
-//   seekWallStraight();
-//   for (;;) {
-//     Motors(left_speed, right_speed);
-//     RINGO_STATE = CLEAR_s;
-//     SimpleGyroNavigation();
-//     edge = LookForEdge();
-//     if (LeftDetected(edge) || RightDetected(edge)) {
-//       OnEyes(100, 0, 0);
-//       Motors(0, 0);
-//       vTaskDelay(100/portTICK_PERIOD_MS);
-//       // pidTurn(-90);
-//       backupTurn();
-//       vTaskDelay(loopHold/portTICK_PERIOD_MS);
-//       RINGO_STATE = OBSTACLE_s;
-//       edge_reset_flag = true;
-//     } 
-//     if (edge_reset_flag) {
-//       left_speed = 60;
-//       right_speed = 75;
-//       edge_reset_flag = false;
-//     } else if (until_ramp_counter < 0) {
-//       right_speed += ramp_turn_radius;
-//       left_speed -= ramp_turn_radius;
-//       right_speed = max(min(right_speed, max_allowed), min_allowed);
-//       left_speed = max(min(left_speed, max_allowed), min_allowed);
-//       until_ramp_counter = 100;
-//       LeftEye(0, left_speed/2, 0);
-//       RightEye(0, right_speed/2, 0);
-//     } else {
-//       until_ramp_counter--;
-//     }
-//     vTaskDelay(loopHold/portTICK_PERIOD_MS);
-//   }
-// }
 
 void loop(){ 
 } // end of loop() function
