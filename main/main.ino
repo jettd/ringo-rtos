3263ec0b93d72aaed7dad0fea2e87f3c600377f2
Jett Durante <jdurante2@macbook-pro-153.unl.edu>
2024-04-12 12:55:41 -0500

    save of timing method

--- main/main.ino
+++ main/main.ino
@@ -3,9 +3,9 @@
 #include "Navigation.h"
 
 volatile char edge;
-
-void TaskSense(void *pvParameters);
-void TaskMotorControl(void * pvParameters);
+int max_time = 0;
+int accumulated_time = 0;
+int num_samples = 0;
 
 void setup(){
   HardwareBegin();        //initialize Ringo's brain to work with his circuitry
@@ -16,68 +16,94 @@ void setup(){
   PlayStartChirp();
   NavigationBegin();
   SwitchButtonToPixels();
-  SetAllPixelsRGB(100, 0, 100);
+  Serial.begin(9600);
+}
 
-  xTaskCreate(TaskMotorControl, "motors", 128, NULL, 10, NULL);
-  xTaskCreate(TaskSense, "sense", 128, NULL, 30, NULL);
+void loop(){ 
+  int start;
+  int end;
+  int t;
+  SwitchAmbientToEdge();
+  // seekWallStraight();
+  for (int i=0; i < 500; i++) {
+    SetPixelRGB(TAIL_BOTTOM, 50, 50, 50);
+    edge = LookForEdge();
+    delay(10);
+    SetPixelRGB(TAIL_BOTTOM, 0, 0, 0);
+    if (LeftDetected(edge) || RightDetected(edge)) {
+      // start = millis();
+      backwards();
+      // end = millis();
+      // t = end-start;
+      // accumulated_time += t;
+      // if (max_time < t) {
+      //   max_time = t;
+      // }
+    } else {
+      start = millis();
+      forward();
+      end = millis();
+      t = end-start;
+      accumulated_time += t;
+      if (max_time < t) {
+        max_time = t;
+      }
+      num_samples += 1;
+    }
+  }
+  Motors(0, 0);
+  SetPixelRGB(TAIL_TOP, 50, 0, 50);
+  // Serial.println(accumulated_time);
+  // Serial.println(max_time);
+  for (int j = 0; j < (accumulated_time/num_samples); j++) {
+    SetPixelRGB(TAIL_TOP, 0, 0, 60);
+    delay(200);
+    SetPixelRGB(TAIL_TOP, 0, 0, 0);
+    delay(200);
+  }
+  delay(100000);
+}// end of loop() function
+
+void forward() {\
+  int left_speed = 90;
+  int right_speed = 120;
+  SetPixelRGB(TAIL_TOP, 0, 50, 0);
+  Motors(left_speed, right_speed);
+  delay(5);
+}
+
+void backwards() {
+  SetPixelRGB(TAIL_TOP, 50, 0, 0);
+  Motors(0, 0);
+  delay(10);
+  backupTurn();
+  delay(10);
 }
 
 void backupTurn() {
-  // OnEyes(50, 50, 50)
   Motors(-30, -65);
-  vTaskDelay(300/portTICK_PERIOD_MS);
+  delay(300);
   Motors(0, 0);
 }
 
-void seekWallStraight() {
-  // OnEyes(50, 50, 50);
-  Motors(100, 100);
-  edge = LookForEdge();
-  while (!LeftDetected(edge) && !RightDetected(edge)) {
-    edge = LookForEdge();
-  }
-  Motors(0, 0);
-}
-
-void TaskSense(void* pvParameters) {
-  int loop_delay = 10; //5-15 seems to work good with the current setup
-  bool toggle = true;
-  SwitchAmbientToEdge();
-  ResetLookAtEdge();
-
-  for (;;) {
-        if (toggle) {
-      toggle = false;
-      SetAllPixelsRGB(50, 0, 0);
-    } else {
-      toggle = true;
-      SetAllPixelsRGB(0, 0, 50);
-    }
-    edge = LookForEdge();
-    vTaskDelay(loop_delay/portTICK_PERIOD_MS);
-    SetAllPixelsRGB(0,0,0);
-  }
-}
-
-void TaskMotorControl(void *pvParameters) {
-  bool toggle = true;
-  Motors(50, 65);
-  vTaskDelay(1000/portTICK_PERIOD_MS);
-  for (;;) {
-    if (LeftDetected(edge) || RightDetected(edge)) {
-      backupTurn();
-    } else {
-      Motors(60, 90);
-    }
-    vTaskDelay(50/portTICK_PERIOD_MS);
-    SetAllPixelsRGB(0,0,0);
-  }
-}
-
-
-void loop(){ 
-} // end of loop() function
-
-
-
+//ramp up speed over time code
+    // if (edge_reset_flag) {
+    //   left_speed = 60;
+    //   right_speed = 75;
+    //   edge_reset_flag = false;
+    // } else if (until_ramp_counter < 0) {
+    //   right_speed += ramp_turn_radius;
+    //   left_speed -= ramp_turn_radius;
+    //   right_speed = max(min(right_speed, max_allowed), min_allowed);
+    //   left_speed = max(min(left_speed, max_allowed), min_allowed);
+    //   until_ramp_counter = 100;
+    //   LeftEye(0, left_speed/2, 0);
+    //   RightEye(0, right_speed/2, 0);
+    // } else {
+    //   until_ramp_counter--;
+    //   Motors(60, 90);
+    // }
+    // vTaskDelay(loopHold/portTICK_PERIOD_MS);
+    // vTaskDelay(50/portTICK_PERIOD_MS);
+    // SetAllPixelsRGB(0,0,0);
 
