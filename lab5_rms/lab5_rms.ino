09a45c8ea588360e7c1070833dc94d68882e8200
Jett Durante <jdurante2@MacBook-Pro-153.home>
2024-04-19 18:06:32 -0500

    timing implemented and timed

--- lab5_rms/lab5_rms.ino
+++ lab5_rms/lab5_rms.ino
@@ -1,8 +1,9 @@
 #include <Arduino_FreeRTOS.h>
 #include "RingoHardware.h"
 #include "Navigation.h"
+#include <Arduino.h>
 
-// Task handles
+
 TaskHandle_t TaskSenseHandle;
 TaskHandle_t TaskMotorHandle;
 
@@ -10,14 +11,16 @@ TaskHandle_t TaskMotorHandle;
 const TickType_t TaskSensePeriod = pdMS_TO_TICKS(20);
 const TickType_t TaskMotorPeriod = pdMS_TO_TICKS(25);
 
-// Task prototypes
 void TaskSense(void *pvParameters);
 void TaskMotor(void *pvParameters);
 
 volatile char edge;
 
+unsigned long total = 0;  //used to time the execution time
+volatile int x = 0;
+
 void setup() {
-  HardwareBegin();        //initialize Ringo's brain to work with his circuitry
+  HardwareBegin(); 
   SwitchMotorsToSerial();
   RestartTimer();
   RxIRRestart(4);
@@ -25,8 +28,6 @@ void setup() {
   PlayStartChirp();
   NavigationBegin();
   SwitchButtonToPixels();
-
-  // Create tasks
   xTaskCreate(TaskSense, "sense", configMINIMAL_STACK_SIZE, NULL, 2, &TaskSenseHandle);
   xTaskCreate(TaskMotor, "motors", configMINIMAL_STACK_SIZE, NULL, 1, &TaskMotorHandle);
 }
@@ -34,38 +35,21 @@ void setup() {
 void loop() {
 }
 
-void TaskSense(void *pvParameters) {
+void TaskSense(void *pvParameters) { //1.4 ms average execution time; 8 runs 800 samples
   TickType_t xLastWakeTime = xTaskGetTickCount();
   ResetLookAtEdge();
-  int a[100];
-  int b[100];
-  int averageTimeMs
 
-  // while (1) {
-  //   edge = LookForEdge();
-  //   vTaskDelayUntil(&xLastWakeTime, TaskSensePeriod);
-  // }
-  for (int i = 0; i < 100; i++) {
-    b = millis();
+  while (1) {
     edge = LookForEdge();
-    a = millis();
     vTaskDelayUntil(&xLastWakeTime, TaskSensePeriod);
-  }
-  vTaskSuspend(&TaskMotorHandle);
-  PlayStartChirp();
-  delay(1000);
-  for (int i = 0; i < 100; i++) {
-    averageTimeMs += (a[i] + b[i])/200
-  }
-  for (int i = 0; i < averageTimeMs; i++) {
-    OnEyes(50, 50, 50);
-    delay(100);
   }
 }
 
-void TaskMotor(void *pvParameters) {
+void TaskMotor(void *pvParameters) { //average ms = 1.8; ran 8x, 800 samples
   TickType_t xLastWakeTime = xTaskGetTickCount();
   int continue_back_counter = 0;
+  int a;
+  int b;
 
   while (1) {
     SetAllPixelsRGB(0, 0, 50);
@@ -92,4 +76,97 @@ void frontArc(void) {
 void backArc(void) {
   Motors(-30, -70);
 }
+
+
+
+/********************************************************************************
+TIMING ESTIMATION IMPLEMENTATION
+*********************************************************************************/
+
+
+// void TaskSense(void *pvParameters) { //1.5
+//   TickType_t xLastWakeTime = xTaskGetTickCount();
+//   ResetLookAtEdge();
+//   unsigned long a, b;
+
+//   while (1) {
+//     b = micros();
+//     edge = LookForEdge();
+//     a = micros();
+//     total += a-b;
+//     vTaskDelayUntil(&xLastWakeTime, TaskSensePeriod);
+//     if (x < 100) {
+//       x += 1;
+//     } else {
+//       // PlayStartChirp();
+//       while (1) {
+//         unsigned long average = total/10000;  //this factor (100 samples) will make the light blink for x times where: x = avg_time_in_microseconds/100 (== time_in_ms * 10)
+//         OffEyes();
+//         // PlayStartChirp();
+//         SwitchSerialToMotors();
+//         vTaskSuspend(TaskMotorHandle);
+//         for (int i = 0; i < average; i++) {
+//           OnEyes(100, 100, 100);
+//           vTaskDelay(250/portTICK_PERIOD_MS);
+//           OffEyes();
+//           vTaskDelay(250/portTICK_PERIOD_MS);
+//         }
+//         while(1) {
+//           Serial.print(average);
+//           vTaskSuspend(TaskSenseHandle);
+//           vTaskDelay(100000);
+//         }
+//       }
+//     }
+//   }
+// }
+
+// void TaskMotor(void *pvParameters) { //average ms = 1.8 200 samples
+//   TickType_t xLastWakeTime = xTaskGetTickCount();
+//   int continue_back_counter = 0;
+//   unsigned long a, b;
+
+//   while (1) {
+//     b = micros();
+//     SetAllPixelsRGB(0, 0, 50);
+//     if (continue_back_counter > 0) {
+//       OnEyes(70, 70, 0);
+//       backArc();
+//       continue_back_counter -= 1;
+//     } else if (LeftDetected(edge) || RightDetected(edge)) {
+//       OnEyes(50, 0, 0);
+//       backArc();
+//       continue_back_counter = 25;
+//     } else {
+//       OnEyes(0, 50, 0);
+//       frontArc();
+//     }
+//     a = micros();
+//     total += a-b;
+//     vTaskDelayUntil(&xLastWakeTime, TaskMotorPeriod);
+//     if (x < 100) {
+//       x += 1;
+//     } else {
+//       // PlayStartChirp();
+//       while (1) {
+//         unsigned long average = total/10000; 
+//         OffEyes();
+//         // PlayStartChirp();
+//         SwitchSerialToMotors();
+//         vTaskSuspend(TaskSenseHandle);
+//         for (int i = 0; i < average; i++) {
+//           OnEyes(100, 100, 100);
+//           vTaskDelay(100/portTICK_PERIOD_MS);
+//           OffEyes();
+//           vTaskDelay(200/portTICK_PERIOD_MS);
+//         }
+//         while(1) {
+//           Serial.print(average);
+//           vTaskSuspend(TaskMotorHandle);
+//           vTaskDelay(100000);
+//         }
+//       }
+//     }
+//   }
+// }
 
